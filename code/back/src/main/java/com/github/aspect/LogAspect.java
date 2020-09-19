a11fb22c13a746ccd553231a1d412dc39f72273b
mins <wbminions@gmail.com>
2020-09-19 22:35:43 +0800

    add cache

--- code/back/src/main/java/com/github/aspect/LogAspect.java
+++ code/back/src/main/java/com/github/aspect/LogAspect.java
@@ -1,18 +1,11 @@
 package com.github.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 
 /**
  * before(前置通知)： 在方法开始执行前执行
@@ -27,25 +20,39 @@ public class LogAspect {
     private final Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
     //1.定义切点
-    @Pointcut(value = "@annotation(log)")//切入点描述 这个是controller包的切入点
+    @Pointcut(value = "@annotation(com.github.annotation.Log)")//切入点描述 这个是controller包的切入点
     public void pointcut(){}//签名，可以理解成这个切入点的一个名称
 
-    @Before("pointcut()") //在切入点的方法run之前要干的
-    public void logBeforeController(JoinPoint joinPoint) {
+    @Before("pointcut()")
+    public void beginTransaction() {
+        System.out.println("before beginTransaction");
+    }
 
+    @After("pointcut()")
+    public void commit() {
+        System.out.println("after commit");
+    }
 
-        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();//这个RequestContextHolder是Springmvc提供来获得请求的东西
-        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+    @AfterReturning(value = "pointcut()", returning = "returnObject")
+    public void afterReturning(JoinPoint joinPoint, Object returnObject) {
+        System.out.println("afterReturning");
+    }
 
-        // 记录下请求内容
-        logger.info("################URL : " + request.getRequestURL().toString());
-        logger.info("################HTTP_METHOD : " + request.getMethod());
-        logger.info("################IP : " + request.getRemoteAddr());
-        logger.info("################THE ARGS OF THE CONTROLLER : " + Arrays.toString(joinPoint.getArgs()));
+    @AfterThrowing("pointcut()")
+    public void afterThrowing() {
+        System.out.println("afterThrowing afterThrowing  rollback");
+    }
 
-        //下面这个getSignature().getDeclaringTypeName()是获取包+类名的   然后后面的joinPoint.getSignature.getName()获取了方法名
-        logger.info("################CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        //logger.info("################TARGET: " + joinPoint.getTarget());//返回的是需要加强的目标类的对象
-        //logger.info("################THIS: " + joinPoint.getThis());//返回的是经过加强后的代理类的对象
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            System.out.println("around");
+            return joinPoint.proceed();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            System.out.println("around");
+        }
     }
 }
